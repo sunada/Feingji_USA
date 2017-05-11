9a7fc54284ae990245569f5f4af6424314e21e60
icemoon1987 <panwenhai1987@163.com>
2017-05-11 15:41:08 +0800

    finished calculate all features

---
+++ icemoon1987/account.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################
+#
+# File Name:  account.py
+#
+# Function:   
+#
+# Usage:  
+#
+# Input:  
+#
+# Output:	
+#
+# Author: panwenhai
+#
+# Create Time:    2017-05-11 13:49:12
+#
+######################################################
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import os
+import time
+import logging
+from datetime import datetime, timedelta
+
+
+class Stock(object):
+
+    def __init__(self, id):
+
+        self.id = id
+        self.share = 0
+        self.value = 0.0
+
+        return
+
+
+    def buy(self, price, share):
+
+        self.share += share
+        self.value = price * share
+
+        return
+
+
+    def sell(self, price, share):
+        
+        self.share -= share
+        self.value = price * share
+
+        return
+
+    
+    def get_mean_price(self):
+        return self.value / self.share
+        
+
+
+class Account(object):
+
+    def __init__(self, cash=0, min_trade_share=0, trade_unit=1):
+
+        self.stock_map = {}
+        self.cash = cash
+        self.min_trade_share = min_trade_share
+        self.trade_unit = trade_unit
+
+        return
+
+
+    def buy(self, stock_id, price, share):
+
+        max_share = int(self.cash / price)
+
+        if share > max_share:
+            logging.warn("cash=%f, price=%f, max_share=%d, share=%d, can only buy %d share." % (self.cash, price, max_share, share, max_share))
+            share = max_share
+
+        if share < self.min_trade_share:
+            logging.warn("share=%d, min_trade_share=%d, can not buy any share." % (share, self.min_trade_share))
+            return 0
+
+        if share % self.trade_unit != 0:
+            true_share = int(share / self.trade_unit) * self.trade_unit
+            logging.warn("share=%d, trade_unit=%d, can only buy %d share." % (share, self.trade_unit, true_share))
+            share = true_share
+
+        if stock_id not in self.stock_map:
+            self.stock_map[stock_id] = Stock(stock_id)
+
+        self.stock_map[stock_id].buy(price, share)
+        self.cash -= price * share
+
+        return share
+
+
+    def sell(self, stock_id, price, share):
+
+        if stock_id not in self.stock_map:
+            logging.warn("not holding this stock. share_id = %s" % (stock_id))
+            return 0
+
+        if share < self.min_trade_share:
+            logging.warn("share=%d, min_trade_share=%d, can not sell any share." % (share, self.min_trade_share))
+            return 0
+
+        if share % self.trade_unit != 0:
+            true_share = int(share / self.trade_unit) * self.trade_unit
+            logging.warn("share=%d, trade_unit=%d, can only sell %d share." % (share, self.min_trade_share, true_share))
+            share = true_share
+
+        if share > self.stock_map[stock_id].share:
+            logging.warn("share=%d, holding_share=%d, can only sell %d share." % (share, self.stock_map[stock_id].share, self.stock_map[stock_id].share))
+            share = self.stock_map[stock_id].share
+
+        self.stock_map[stock_id].sell(price, share)
+        self.cash += price * share
+
+        if self.stock_map[stock_id].share == 0:
+            del self.stock_map[stock_id]
+        
+        return share
+
+
+    def get_value(self):
+
+        value = 0
+
+        for stock_id in self.stock_map:
+            value += self.stock_map[stock_id].value
+
+        value += self.cash
+
+        return value
+
+
+    def __str__(self):
+
+        result = ""
+
+        for stock_id in self.stock_map:
+            result += "stock_id=%s, share=%d, value=%f\n" % ( self.stock_map[stock_id].id, self.stock_map[stock_id].share, self.stock_map[stock_id].value)
+
+        result += "cash=%f\n" % (self.cash)
+        result += "value=%f\n" % (self.get_value())
+
+        return result
+
+
+if __name__ == "__main__":
+
+    account = Account(cash=100000, min_trade_share=100, trade_unit=100)
+
+    print "normal: buy aaa"
+    account.buy("aaa", 10.0, 500)
+    print str(account)
+
+    print "normal: buy bbb"
+    account.buy("bbb", 15.0, 200)
+    print str(account)
+
+    print "normal: sell aaa"
+    account.sell("aaa", 20.0, 500)
+    print str(account)
+
+    print "normal: sell bbb"
+    account.sell("bbb", 30.0, 100)
+    print str(account)
+
+    print "normal: sell bbb"
+    account.sell("bbb", 30.0, 100)
+    print str(account)
+
+    account = Account(cash=100000, min_trade_share=100, trade_unit=100)
+
+    print "buy share > max share"
+    account.buy("aaa", 10.0, 999999999)
+    print str(account)
+
+    print "sell > holding share"
+    account.sell("aaa", 10.0, 999999999)
+    print str(account)
+
+    print "buy share < min_trade_share"
+    account.buy("aaa", 10.0, 99)
+    print str(account)
+
+    print "buy share % trade_unit != 0"
+    account.buy("aaa", 10.0, 113)
+    print str(account)
+
+    print "sell share % trade_unit != 0"
+    account.sell("aaa", 10.0, 113)
+    print str(account)
+
+    print "buy aaa"
+    account.sell("aaa", 10.0, 100)
+    print str(account)
+
+    print "sell share < min_trade_share"
+    account.sell("aaa", 10.0, 99)
+    print str(account)
+
+    print "sell unhold stock"
+    account.sell("bbb", 10.0, 100)
+    print str(account)
+
+
--- icemoon1987/main.py
+++ icemoon1987/main.py
@@ -30,6 +30,9 @@ import shutil
 import pandas as pd
 from datetime import datetime, timedelta
 
+# 设置pandas数据显示宽度
+pd.set_option("display.width", 300)
+
 
 def init(conf_file):
     """ 初始化函数，根据配置文件，初始化各个目录，返回配置结构 """
@@ -52,92 +55,394 @@ def init(conf_file):
     else:
         log_level = logging.INFO
 
-    logging.basicConfig(level=log_level, filename="%s/log.%s" % (log_dir, datetime.now().strftime("%Y%m%d")), filemode='a', \
-                                    format='%(asctime)s [%(levelname)s] [%(filename)s] [%(funcName)s] [%(lineno)d] %(message)s')
+    logging.basicConfig(level=log_level, filename="%s/log.%s" % (log_dir, datetime.now().strftime("%Y%m%d")), filemode='a', format='%(asctime)s [%(levelname)s] [%(filename)s] [%(funcName)s] [%(lineno)d] %(message)s')
+    #logging.basicConfig(level=log_level, format='%(asctime)s [%(levelname)s] [%(filename)s] [%(funcName)s] [%(lineno)d] %(message)s')
 
     return config_json
     
 
 def load_trade_data(data_dir):
-    """ 加载一个目录下的所有详情数据，返回DataFrame """
+    """ 加载一个目录下的所有价格详情数据，返回DataFrame """
 
     result = pd.DataFrame()
 
     for file_name in os.listdir(data_dir):
-        tmp = pd.read_csv(data_dir + "/" + file_name)
+        tmp = pd.read_csv(data_dir + "/" + file_name, parse_dates=["Date"])
         result = pd.concat([result, tmp], ignore_index=True)
 
     return result
 
 
 def load_dividends(dividen_file):
-    """ 加载dividend数据，返回DataFrame """
+    """ 加载红利数据，返回DataFrame """
 
-    return pd.read_csv(dividen_file)
+    return pd.read_csv(dividen_file, parse_dates=["Declared Date", "Payable Date", "Ex Date"])
 
 
 def load_sponsor(sponsor_file):
-    """ 加载sponsor数据，返回DataFrame """
+    """ 加载基金公司数据，返回DataFrame """
 
     return pd.read_csv(sponsor_file)
 
 
-def filter_by_sponsor(trade_data, sponsor, config_json):
-    """ 按照sponsor过滤数据 """
+def filter_by_sponsor(trade_data, sponsor, select_sponsors):
+    """ 按照基金公司过滤数据 """
 
-    tickers = sponsor.where(sponsor["sponsor"].isin(config_json["select_sponsors"])).dropna()["ticker"].values
+    tickers = sponsor.where(sponsor["sponsor"].isin(select_sponsors)).dropna()["ticker"].values
 
     result = trade_data.where(trade_data["Ticker"].isin(tickers)).dropna()
 
     return result
 
 
-def generate_earnings(trade_data, config_json):
-    """ 计算earnings """
+def mark_special_dividend(dividend):
+    """ 将分红数据按日期排序，并标注是否为特殊分红，通过前后对比。需要输入按照Ticker、Ex Date排序 """
 
-    print trade_data
-    print config_json["earning_days_gap"]
+    result = result.ix[:, ["Ticker", "Ex Date", "Distrib Amount"]]
 
-    return
+    result["last_amount"] = result.shift(1)["Distrib Amount"]
+    result["amount_raise"] = (result["Distrib Amount"] - result["last_amount"]) / result["last_amount"]
+
+    result["is_special"] = 0
+
+    result.loc[result["amount_raise"] > 0.5, ["is_special"]] = 1
+    result.loc[result["amount_raise"] < -0.5, ["is_special"]] = 1
+
+    result.loc[result["Ticker"] != result["Ticker"].shift(1), ["is_special"]] = 0
+
+    return result
 
 
+def mark_special_dividend_by_mean(dividend):
+    """ 将分红数据按日期排序，并标注是否为特殊分红，通过平均值对比。需要输入按照Ticker、Ex Date排序 """
+
+    tmp = dividend.ix[:, ["Ticker", "Ex Date", "Distrib Amount"]]
+
+    # 计算分红平均值，合并入原来的数据
+    mean_result = tmp.groupby(tmp["Ticker"]).mean()
+    mean_result.reset_index(level=0, inplace=True)
+    mean_result.rename(columns={"Distrib Amount": "mean_amount"}, inplace=True)
+
+    dividend = pd.merge(dividend, mean_result, how="left", left_on="Ticker", right_on="Ticker")
+
+    # 判断是否为特殊分红
+    dividend["amount_raise"] = (dividend["Distrib Amount"] - dividend["mean_amount"]) / dividend["mean_amount"]
+
+    dividend["is_special"] = 0
+
+    dividend.loc[dividend["amount_raise"] > 0.8, ["is_special"]] = 1
+    dividend.loc[dividend["amount_raise"] < -0.8, ["is_special"]] = 1
+
+    return dividend
 
 
-def load_data(config_json):
-    """ 加载数据，合并数据 """
+def mark_dividend_down(dividend):
+    """ 标注每次分红是否比前一次分红低，除去特殊分红。需要输入按照Ticker、Ex Date排序，且已标注特殊分红"""
 
-    trade_data = load_trade_data(config_json["data_dir"])
-    dividend = load_dividends("./conf/dividends.csv")
-    sponsor = load_sponsor("./conf/ticker_sponsor.csv")
+    # 过滤特殊分红
 
-    #print trade_data
-    #print dividend
-    #print sponsor
+    tmp = dividend.where(dividend["is_special"] == 0).dropna()
+    tmp = tmp.ix[:, ["Ticker", "Ex Date", "Distrib Amount"]]
 
-    #print trade_data.where(trade_data["Ticker"] == "MMV").dropna()
-    #print trade_data.query("Ticker == \"MFM\"")
-    #print sponsor.query("ticker == \"MMV\"")
+    # 标注是否比前一次分红低
+    tmp["last_amount"] = tmp.shift(1)["Distrib Amount"]
 
-    #result = pd.merge(trade_data, sponsor, how="left", left_on="Ticker", right_on="ticker")
+    tmp["is_low"] = 0
+    tmp.loc[tmp["Distrib Amount"] < tmp["last_amount"], ["is_low"]] = 1
 
-    #print result
+    # 对每个Ticker的第一条数据进行修正
+    tmp.loc[tmp["Ticker"] != tmp["Ticker"].shift(1), ["last_amount", "is_low"]] = 0
+
+    dividend = pd.merge(dividend, tmp, how="left", left_on=["Ticker", "Ex Date", "Distrib Amount"], right_on=["Ticker", "Ex Date", "Distrib Amount"])
+
+    # 对特殊分红数据进行修正
+    dividend.loc[dividend["is_special"] == 1, ["last_amount", "is_low"]] = 0
+
+    return dividend
+
+
+def mark_period_dividend_rise(dividend, dividend_rise_days):
+    """ 标注一段时间之内，每次分红是否都比上一次分红高"""
+
+    # 分组使用移动窗口统计是否分红不降
+    grouped = dividend.groupby(dividend["Ticker"])
+
+    is_low_sum_result = pd.DataFrame()
+
+    for ticker, data in grouped:
+
+        is_low_sum = data.ix[:, ["Ticker", "Ex Date", "is_low"]].rolling(on="Ex Date", window=str(dividend_rise_days) + "d").sum()
+
+        is_low_sum["is_period_rise"] = 0
+
+        is_low_sum.loc[is_low_sum["is_low"] == 0, ["is_period_rise"]] = 1
+
+        is_low_sum_result = pd.concat([is_low_sum_result, is_low_sum])
+
+    dividend = pd.merge(dividend, is_low_sum_result.ix[:, ["is_period_rise"]], how="left", left_index=True, right_index=True)
+
+    return dividend
+
+
+def calcu_period_dividend_earning(dividend, earning_days):
+    """ 计算一段时间之内的分红总收益 """
+
+    # 分组使用移动窗口统计分红总收益
+    grouped = dividend.groupby(dividend["Ticker"])
+
+    dividend_earning = pd.DataFrame()
+
+    for ticker, data in grouped:
+
+        tmp = data.ix[:, ["Ticker", "Ex Date", "Distrib Amount"]].rolling(on="Ex Date", window=str(earning_days) + "d").sum()
+
+        dividend_earning = pd.concat([dividend_earning, tmp])
+
+    dividend_earning.rename(columns={"Distrib Amount": "amount_sum"}, inplace=True)
+
+    dividend = pd.merge(dividend, dividend_earning.ix[:, ["amount_sum"]], how="left", left_index=True, right_index=True)
+
+    return dividend
+
+
+def calcu_dividend_freq(dividend):
+    """ 计算非特殊分红频率，一年中分红次数。需要输入已标注特殊分红 """
+
+    # 过滤特殊分红，将日期转为年份
+    tmp = dividend.where(dividend["is_special"] == 0).dropna()
+    tmp["year"] = tmp["Ex Date"].apply(lambda x: x.date().year)
+
+    # 过滤2010年、2017年的不完整数据
+    tmp = tmp.where(tmp["year"] != 2010).dropna()
+    tmp = tmp.where(tmp["year"] != 2017).dropna()
+
+    # 统计每个基金分红了几年
+    tmp2 = tmp.ix[:, ["Ticker", "year"]].drop_duplicates()
+    ticker_year = tmp2.groupby(tmp2["Ticker"]).count()
+    ticker_year.rename(columns={"year": "year_num"}, inplace=True)
+    ticker_year.reset_index(level=0, inplace=True)
+
+    # 统计分红次数
+    ticker_dividend_num = tmp.ix[:, ["Ticker"]].groupby(tmp["Ticker"]).count()
+    ticker_dividend_num.rename(columns={"Ticker": "dividend_num"}, inplace=True)
+    ticker_dividend_num.reset_index(level=0, inplace=True)
+
+    # 合并，并计算年均分红次数
+    merge_result = pd.merge(ticker_year, ticker_dividend_num)
+    dividend = pd.merge(dividend, merge_result, how="left")
+    dividend["mean_dividend_num"] = dividend["dividend_num"] / dividend["year_num"]
+
+    return dividend
+
+
+def calcu_dividend_NTM(dividend):
+
+    dividend["NTM"] = dividend["Distrib Amount"] * dividend["mean_dividend_num"]
+
+    return dividend
+
+
+def calcu_earnings(trade_data, earning_days):
+    """ 计算一段时间之内的差价回报，结束价格 - 开始价格 """
+
+    # 分组处理每一个封基
+    grouped = trade_data.groupby(trade_data["Ticker"])
+
+    earnings = pd.DataFrame()
+
+    for ticker, data in grouped:
+
+        # 由于本身数据有日期“漏洞”，需要经过重采样，填补漏洞
+        # 重采样需要使用日期作为index
+        data.set_index("Date", inplace=True)
+
+        tmp = data.resample("D").pad()
+
+        # 计算差价回报
+        tmp["pre_price"] = tmp.shift(earning_days)["Share Price"]
+        tmp["price_earning"] = tmp["Share Price"] - tmp["pre_price"]
+
+        earnings = pd.concat([earnings, tmp])
+
+    earnings.fillna(0, inplace=True)
+    earnings.reset_index(level=0, inplace=True)
+
+    trade_data = pd.merge(trade_data, earnings.ix[:, ["Ticker", "Date", "pre_price", "price_earning"]], how="left", left_on=["Ticker", "Date"], right_on=["Ticker", "Date"])
+
+    return trade_data
+
+
+def calcu_premium_rate(trade_data, z_score_days):
+    """ 计算溢价率，(当日价格 - 前一日净值) / 前一日净值 """
+
+    # 将前一天的净值，合并到这一天的数据中
+    trade_data["last_value"] = trade_data.shift(1)["Net Asset Value"]
     
+    # 一个Ticker的第一天，无法取到前一天的净值，过滤掉
+    result = trade_data.where(trade_data["Ticker"] == trade_data["Ticker"].shift(1)).dropna()
+
+    # 计算溢价率
+    result["premium_rate"] = result["Share Price"] / result["last_value"] - 1
+
+    # 分组使用移动窗口计算溢价率均值、溢价率标准差
+    grouped = result.groupby(result["Ticker"])
+
+    premium_result = pd.DataFrame()
+
+    for ticker, data in grouped:
+
+        premium_rate_mean = data.ix[:, ["Ticker", "Date", "premium_rate"]].rolling(on="Date", window= str(z_score_days) + "d").mean()
+        premium_rate_mean.rename(columns={"premium_rate": "premium_rate_mean"}, inplace=True)
+
+        premium_rate_std = data.ix[:, ["Ticker", "Date", "premium_rate"]].rolling(on="Date", window= str(z_score_days) + "d").std()
+        premium_rate_std.rename(columns={"premium_rate": "premium_rate_std"}, inplace=True)
+
+        premium_result = pd.concat([premium_result, pd.merge(premium_rate_mean, premium_rate_std)])
+
+    result = pd.merge(result, premium_result, how="left", left_on=["Ticker", "Date"], right_on=["Ticker", "Date"])
+
+    # 计算溢价率z-score
+    result["premium_rate_z_score"] = (result["premium_rate"] - result["premium_rate_mean"]) / result["premium_rate_std"]
+
+    return result
+
+
+def process_dividend(dividend, config_json):
+    """ 处理分红数据 """
+
+    # 提取需要的字段
+    dividend = dividend.ix[:, ["Ticker", "Ex Date", "Payable Date", "Distrib Amount"]]
+
+    # 按照基金、日期排序
+    dividend = dividend.sort_values(by=["Ticker", "Ex Date"])
+
+    # 标注是否为特殊分红
+    dividend = mark_special_dividend_by_mean(dividend)
+
+    # 标注每次分红是否比前一次低
+    dividend = mark_dividend_down(dividend)
+
+    # 标注一段时间之内，每次分红是否保持不降
+    dividend = mark_period_dividend_rise(dividend, config_json["dividend_rise_days"])
+
+    # 计算一段时间之内的分红总收益
+    dividend = calcu_period_dividend_earning(dividend, config_json["earning_days"])
+
+    # 计算分红频率
+    dividend = calcu_dividend_freq(dividend)
+
+    # 计算每天的NTM
+    dividend = calcu_dividend_NTM(dividend)
+
+    return dividend
+
+
+def process_trade_data(trade_data, sponsor, config_json):
+    """ 处理交易详情数据 """
+
+    # 按照基金名、日期排序
+    trade_data = trade_data.sort_values(by=["Ticker", "Date"])
+
+    # 按照基金公司过滤
+    trade_data = filter_by_sponsor(trade_data, sponsor, config_json["select_sponsors"])
+
+    # 计算溢价率、溢价率z-score
+    trade_data = calcu_premium_rate(trade_data, config_json["z_score_days"])
+
+    # 计算一段时间之内的差价回报
+    trade_data = calcu_earnings(trade_data, config_json["earning_days"])
+
+    return trade_data
+
+
+def get_test_dates(trade_data, dividend, config_json):
+
+    logging.debug("dividend_rise_days: %d" % (config_json["dividend_rise_days"]))
+    logging.debug("earning_days: %d" % (config_json["earning_days"]))
+    logging.debug("z_score_days: %d" % (config_json["z_score_days"]))
+    logging.debug("backtest_start_date: %s" % (config_json["backtest_start_date"]))
+    logging.debug("backtest_end_date:   %s" % (config_json["backtest_end_date"]))
+
+    # 取出交易数据和红利数据中的交集部分
+    max_date1 = dividend["Ex Date"].max()
+    max_date2 = trade_data["Date"].max()
+
+    if max_date1 > max_date2:
+        max_date = max_date2
+    else:
+        max_date = max_date1
+
+    min_date1 = dividend["Ex Date"].min()
+    min_date2 = trade_data["Date"].min()
+
+    if min_date1 > min_date2:
+        min_date = min_date1
+    else:
+        min_date = min_date2
+
+    logging.info("date range in data:  %s   --  %s" % (min_date, max_date))
+
+    # 计算指标时需要的数据天数
+    tmp = []
+    tmp.append(config_json["dividend_rise_days"])
+    tmp.append(config_json["earning_days"])
+    tmp.append(config_json["z_score_days"])
+
+    max_days = max(tmp)
+    logging.info("days to calculate all features:  %d" % (max_days))
+
+    # 检查回测开始、结束时间是否合理
+    if config_json.get("backtest_start_date", "") == "":
+        backtest_start_date = min_date + timedelta(days = max_days)
+        logging.warn("backtest_start_date not found. Using default: %s" % (backtest_start_date))
+    else:
+        backtest_start_date = datetime.strptime(config_json["backtest_start_date"], "%Y-%m-%d")
+
+    if config_json.get("backtest_end_date", "") == "":
+        backtest_end_date = max_date
+        logging.warn("backtest_end_date not found. Using default: %s" % (backtest_end_date))
+    else:
+        backtest_start_date = datetime.strptime(config_json["backtest_start_date"], "%Y-%m-%d")
+
+    if backtest_start_date + timedelta(days=max_days) > backtest_end_date:
+        logging.fatal("Not enouth data to run backtest!")
+        return None, None
+
+    return backtest_start_date, backtest_end_date
+
+
+def backtest(trade_data, dividend, config_json):
+
+    start_date, end_date = get_test_dates(trade_data, dividend, config_json)
+
+    if start_date == None:
+        return None
+
     return
+
 
 
 def main():
 
     config_json = init("./conf/config.json")
 
-    #load_data(config_json)
-
+    # 加载数据
     trade_data = load_trade_data(config_json["data_dir"])
     dividend = load_dividends("./conf/dividends.csv")
     sponsor = load_sponsor("./conf/ticker_sponsor.csv")
 
-    filtered_trade_data = filter_by_sponsor(trade_data, sponsor, config_json)
+    # 处理分红数据
+    #dividend = process_dividend(dividend, config_json)
+    logging.debug("finish process_dividend.")
 
-    generate_earnings(trade_data, config_json)
+    # 处理交易详情数据
+    #trade_data = process_trade_data(trade_data, sponsor, config_json)
+    logging.debug("finish process_trade_data.")
+
+    # 回测
+    logging.info("backtest start at: %s" % (datetime.now()))
+    backtest(trade_data, dividend, config_json)
+    logging.info("backtest end at: %s" % (datetime.now()))
 
     return
 
